9dd6c638575d20a95eaf40207e6fccacb8eac78e
Viktor Zavodsky <hello@0xvector.me>
2026-05-06 10:43:21 +0200

    Make DAP step request not resume implicitly

--- tests/system/dap-simple-mips32/scenario_csr.py
+++ tests/system/dap-simple-mips32/scenario_csr.py
@@ -18,8 +18,10 @@ for csr in (BADVADDR, ENTRYHI, CAUSE, EPC, CONFIG, LLADDR):
 
 # Step a bit and check that the PC is updated, but registers are unchanged
 adapter.send(StepRequest, DEFAULT_CPU, 2).expect_response(StatusOk)
+adapter.send(ResumeRequest).expect_response()
 adapter.expect_event(StoppedAtEvent, DEFAULT_CPU, RST_VEC_VIRT + 2 * INSTR_LEN, StoppedReasonStep)
 adapter.send(StepRequest, DEFAULT_CPU, 2).expect_response(StatusOk)
+adapter.send(ResumeRequest).expect_response()
 adapter.expect_event(StoppedAtEvent, DEFAULT_CPU, RST_VEC_VIRT + 4 * INSTR_LEN, StoppedReasonStep)
 
 for csr in (BADVADDR, ENTRYHI, CAUSE, EPC, CONFIG, LLADDR):
--- tests/system/dap-simple-mips32/scenario_pc.py
+++ tests/system/dap-simple-mips32/scenario_pc.py
@@ -10,6 +10,7 @@ adapter.send(ReadPCRequest, DEFAULT_CPU).expect_response(StatusOk, RST_VEC_VIRT 
 
 # Step once and check that the PC has advanced by another instruction
 adapter.send(StepRequest, DEFAULT_CPU, 1).expect_response(StatusOk)
+adapter.send(ResumeRequest).expect_response()
 adapter.expect_event(StoppedAtEvent, DEFAULT_CPU, RST_VEC_VIRT + 2 * INSTR_LEN, StoppedReasonStep)
 adapter.send(ReadPCRequest, DEFAULT_CPU).expect_response(StatusOk, RST_VEC_VIRT + 2 * INSTR_LEN)
 
@@ -23,9 +24,11 @@ rest = PROGRAM_LEN - half
 for _ in range(5):
     # Step through the first half of the program and check that the PC has advanced correctly
     adapter.send(StepRequest, DEFAULT_CPU, half).expect_response(StatusOk)
+    adapter.send(ResumeRequest).expect_response()
     adapter.expect_event(StoppedAtEvent, DEFAULT_CPU, RST_VEC_VIRT + half * INSTR_LEN, StoppedReasonStep)
     # Step through the rest of the program and check that the PC has advanced correctly
     adapter.send(StepRequest, DEFAULT_CPU, rest).expect_response(StatusOk)
+    adapter.send(ResumeRequest).expect_response()
     adapter.expect_event(StoppedAtEvent, DEFAULT_CPU, RST_VEC_VIRT + PROGRAM_LEN * INSTR_LEN, StoppedReasonStep)
     # Rollback the PC
     adapter.send(WritePCRequest, DEFAULT_CPU, RST_VEC_VIRT).expect_response(StatusOk)
